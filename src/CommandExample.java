247a366c9a6c6599730a33c78fa8a9183a7b28da
solutionman <solutionman@inbox.ru>
2021-03-26 10:58:28 +0700

    examples

---
+++ src/CommandExample.java
@@ -0,0 +1,24 @@
+public class CommandExample {
+    public interface Command
+    {
+        public void execute(Object data);
+    }
+
+    public static class PrintCommand implements Command
+    {
+        public void execute(Object data)
+        {
+            System.out.println(data.toString());
+        }
+    }
+
+    public static void callCommand(Command command, Object data)
+    {
+        command.execute(data);
+    }
+
+    public static void main(String... args)
+    {
+        callCommand(new PrintCommand(), "hello world");
+    }
+}
