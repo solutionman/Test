63461ed44e0cb30f73779f8efe2f390ccc7721bf
solutionman <solutionman@inbox.ru>
2021-04-01 16:36:22 +0700

    CommandExample

--- src/CommandExample.java
+++ src/CommandExample.java
@@ -1,24 +1,19 @@
 public class CommandExample {
-    public interface Command
-    {
+    public interface Command {
         public void execute(Object data);
     }
 
-    public static class PrintCommand implements Command
-    {
-        public void execute(Object data)
-        {
+    public static class PrintCommand implements Command {
+        public void execute(Object data) {
             System.out.println(data.toString());
         }
     }
 
-    public static void callCommand(Command command, Object data)
-    {
+    public static void callCommand(Command command, Object data) {
         command.execute(data);
     }
 
-    public static void main(String... args)
-    {
+    public static void main(String... args) {
         callCommand(new PrintCommand(), "hello world");
     }
 }
