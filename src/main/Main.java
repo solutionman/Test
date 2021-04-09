d5e716873e0dceb6420ede121d96be868cf8f9b3
solutionman <solutionman@inbox.ru>
2021-04-09 22:27:29 +0700

    stream

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,27 +1,37 @@
 package main;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("hello");
-
         String numbers = "12а3";
         Integer sum = calculateSum(numbers);
         System.out.println(sum);
+        Integer sumStream = calculateSumStream(numbers);
+        System.out.println(sumStream);
     }
 
     // input = 123456
-    public static Integer calculateSum(String input){
+    public static Integer calculateSum(String input) {
 
-        String [] asdf = input.split("");
+        String[] asdf = input.split("");
 //        System.out.println(asdf[2]);
-        Integer sum = 0;
-        for(String s : asdf){
+        int sum = 0;
+        for (String s : asdf) {
             try {
                 sum += Integer.parseInt(s);
-            } catch (NumberFormatException e){
-                System.out.println( s + " не является целым числом" );
+            } catch (NumberFormatException e) {
+                System.out.println(s + " не является целым числом");
             }
         }
         return sum;
+    }
+
+    public static Integer calculateSumStream(String input) {
+        return Arrays.stream(input.split(""))
+                .filter(s -> s.matches("\\d+"))
+                .mapToInt(Integer::valueOf)
+                .sum();
     }
 }
