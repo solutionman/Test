5e93459ef6ca7a6d66debfc284107a84d53683bc
solutionman <solutionman@inbox.ru>
2021-04-09 21:02:48 +0700

    string Integer

---
+++ src/feature/Main.java
@@ -0,0 +1,28 @@
+package feature;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+
+
+    public static void main(String[] args){
+
+
+    }
+
+    public Future<String> calculateAsync() throws InterruptedException {
+        CompletableFuture<String> completableFuture = new CompletableFuture<>();
+
+        Executors.newCachedThreadPool().submit(() -> {
+            Thread.sleep(500);
+            completableFuture.complete("Hello");
+            return null;
+        });
+
+        return completableFuture;
+    }
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,22 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+//        System.out.println("hello");
+
+        String numbers = "123";
+        Integer sum = calculateSum(numbers);
+        System.out.println(sum);
+    }
+
+    // input = 123456
+    public static Integer calculateSum(String input){
+
+        String [] asdf = input.split("");
+//        System.out.println(asdf[2]);
+        Integer sum = 0;
+        for(String s : asdf){
+            sum += Integer.parseInt(s);
+        }
+        return sum;
     }
 }
