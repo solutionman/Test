02a0f02c3d3e9b1563942cfdcbd91c79f57bea80
solutionman <solutionman@inbox.ru>
2021-04-23 11:59:15 +0700

    multithreading

--- src/multithread/MainMultiThread.java
+++ src/multithread/MainMultiThread.java
@@ -8,7 +8,12 @@ public class MainMultiThread {
 //        myThread.run(); // это запустит выполнение в текущем потоке
         myThread.start(); // это запустит в отдельном потоке
         anotherThread.start(); // это запустит еще один отдельный поток
-        System.out.println("Message from main method");
+
+        // Еще один способ запуска потока, через Runner implements Runnable
+        Thread thread = new Thread(new Runner());
+        thread.start();
+
+        System.out.println("Main thread message");
     }
 }
 
@@ -16,7 +21,7 @@ class MyThread extends Thread{
     @Override
     public void run(){
         for(int i = 0; i < 1000; i++){
-            System.out.println("thread one " + i);
+            System.out.println("1 MyThread thread message " + i);
         }
     }
 }
@@ -25,7 +30,7 @@ class AnotherThread extends Thread{
     @Override
     public void run(){
         for(int i = 0; i < 1000; i++){
-            System.out.println("thread two " + i);
+            System.out.println("2 Another thread message " + i);
             if(i == 800){
                 try {
                     sleep(3000);
@@ -33,6 +38,15 @@ class AnotherThread extends Thread{
                     e.printStackTrace();
                 }
             }
+        }
+    }
+}
+
+class Runner implements Runnable{
+    @Override
+    public void run() {
+        for(int i = 0; i < 1000; i++){
+            System.out.println("3 Runner thread message " + i);
         }
     }
 }
