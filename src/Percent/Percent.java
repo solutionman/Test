8f61bf1b352418c46065fb52f9540c2233b80ddd
solutionman <solutionman@inbox.ru>
2021-05-06 17:58:16 +0700

    percent

---
+++ src/Percent/Percent.java
@@ -0,0 +1,29 @@
+package Percent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Percent {
+    public static void main(String[] args) {
+        List<List> first = new ArrayList<>();
+        List<String> second = new ArrayList<>();
+        second.add("hello");
+        second.add("helloAgain");
+        first.add(second);
+        String hi = (first.get(0)).get(0).toString();
+        System.out.println(hi);
+
+        List<String> list = new ArrayList();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        int counter = 1;
+        int percent = 68;
+        int part = 0;
+        for (String value : list) {
+            part = counter * 32 / list.size();
+            System.out.println(percent + part);
+            counter++;
+        }
+    }
+}
