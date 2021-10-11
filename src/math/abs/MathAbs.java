9413f9fe7a95a36c2ecd8945b71cd7eb8feefbf7
solutionman <solutionman@inbox.ru>
2021-10-11 11:21:45 +0700

    Math.abs negative value

---
+++ src/math/abs/MathAbs.java
@@ -0,0 +1,9 @@
+package math.abs;
+
+public class MathAbs {
+    public static void main(String[] args) {
+        // Math.abs can print negative number:
+        System.out.println(Math.abs(-500));
+        System.out.println(Math.abs(Integer.MIN_VALUE));
+    }
+}
