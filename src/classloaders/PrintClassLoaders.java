111fd6669869286919304f95c9438fe0655d35d0
solutionman <solutionman@inbox.ru>
2022-07-10 11:18:02 +0700

    ClassLoader

--- src/classloaders/PrintClassLoaders.java
+++ src/classloaders/PrintClassLoaders.java
@@ -1,14 +1,19 @@
 package classloaders;
 
 public class PrintClassLoaders {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        // Bootstrap class loader
         // null - Bootstrap class loader
         System.out.println(String.class.getClassLoader());
 
-        // sun.misc.Launcher$AppClassLoader@4e0e2f2a - System class loader (when run from terminal)
-        // jdk.internal.loader.ClassLoaders$AppClassLoader@3d4eac69 - System class loader
+        // System class loader
+        // sun.misc.Launcher$AppClassLoader@4e0e2f2a (when run from terminal)
+        // jdk.internal.loader.ClassLoaders$AppClassLoader@3d4eac69
         System.out.println(PrintClassLoaders.class.getClassLoader());
+
+        // Extensions class loader
+        // ?????
 
     }
 }
