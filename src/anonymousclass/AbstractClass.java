9e0a9db6032a45f68b164c052673b38596065e5e
solutionman <solutionman@inbox.ru>
2022-07-16 12:43:32 +0700

    anonymous class

---
+++ src/anonymousclass/AbstractClass.java
@@ -0,0 +1,7 @@
+package anonymousclass;
+
+public abstract class AbstractClass {
+    public void doSomethingMethod() {
+        System.out.println("print something");
+    }
+}
--- src/anonymousclass/Main.java
+++ src/anonymousclass/Main.java
@@ -2,6 +2,8 @@ package anonymousclass;
 
 public class Main {
     public static void main(String[] args) {
+
+        // create anonymous class from interface
         SomeInterface anonymousClass = new SomeInterface() {
             private int id;
             private String name;
@@ -9,15 +11,39 @@ public class Main {
             @Override
             public void createSomeCoolClass() {
                 this.id = 1;
+                this.name = "not so cool";
+                System.out.println("id " + this.id);
+                System.out.println("name " + this.name);
             }
 
             {
                 this.name = "reallyCoolClass";
+                System.out.println("id " + this.id);
+                System.out.println("name " + this.name);
             }
 
+            public String getName() {
+                return name;
+            }
         };
 
         anonymousClass.createSomeCoolClass();
         System.out.println(anonymousClass.getClass());
+
+        // create anonymous class from abstract class
+        AbstractClass anonymousClass2 = new AbstractClass() {
+            @Override
+            public void doSomethingMethod() {
+                System.out.println("run from anonymous class hmmm");
+                super.doSomethingMethod();
+            }
+
+            public void cantCallOutside() {
+                System.out.println("never run");
+            }
+        };
+        anonymousClass2.doSomethingMethod();
+        // anonymousClass2.cantCallOutside();  // error
+
     }
 }
