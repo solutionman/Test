cf5a86b737a0d8256e7898020103aa68f925f985
solutionman <solutionman@inbox.ru>
2022-07-17 22:06:31 +0700

    casting

---
+++ src/casting/CastingTest.java
@@ -0,0 +1,23 @@
+package casting;
+
+public class CastingTest {
+
+    public static void main(String[] args){
+
+        int a = 1;
+        long b = a;
+        System.out.println(b);
+
+        b = 2L;
+        a = (int) b;
+        System.out.println(a);
+
+        A classA = new A();
+        Object object = classA;
+        A classA1 = (A) object;
+
+        // ClassCastException
+        B classB = (B) object;
+
+    }
+}
