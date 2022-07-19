98fe8cd01821dd860052664688028773c4147b65
solutionman <solutionman@inbox.ru>
2022-07-19 13:11:28 +0700

    equals

--- src/equals/Third.java
+++ src/equals/Third.java
@@ -3,32 +3,37 @@ package equals;
 public class Third {
     int id;
     String name;
-    void setId(int id){
+
+    void setId(int id) {
         this.id = id;
     }
-    void setName(String name){
+
+    void setName(String name) {
         this.name = name;
     }
-    int getId(){
+
+    int getId() {
         return id;
     }
-    String getName(){
+
+    String getName() {
         return name;
     }
+
     @Override
-    public boolean equals(Object o){
-        if(o == null) return false;
-        if(o == this) return true;
-        if(getClass() != o.getClass()) return false;
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (o == this) return true;
+        if (getClass() != o.getClass()) return false;
         Third third = (Third) o;
-        if(id == third.id && name.equals(third.name)){
+        if (id == third.id && name.equals(third.name)) {
             return true;
         }
         return false;
     }
 
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return id * name.hashCode();
     }
 }
