e6f5947bf24e5bf12c3700d057fc8d962f8fcb2f
solutionman <solutionman@inbox.ru>
2022-07-24 19:56:19 +0700

    Queue

---
+++ src/collections/QueueTest.java
@@ -0,0 +1,31 @@
+package collections;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueTest {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < 5; i++){
+            queue.add(i);
+        }
+
+        // Элементы очереди
+        System.out.println(queue);
+
+        queue.remove();
+        System.out.println(queue);
+        queue.remove();
+        System.out.println(queue);
+        queue.remove();
+        System.out.println(queue);
+        queue.add(5);
+        System.out.println(queue);
+        queue.remove();
+        System.out.println(queue);
+        queue.add(6);
+        System.out.println(queue);
+        queue.remove();
+        System.out.println(queue);
+    }
+}
