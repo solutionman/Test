9f33e08f8010a70abe80f165352520ce90ca6e99
solutionman <solutionman@inbox.ru>
2022-07-24 20:40:28 +0700

    Queue

--- src/collections/QueueTest.java
+++ src/collections/QueueTest.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 public class QueueTest {
     public static void main(String[] args) {
         Queue<Integer> queue = new LinkedList<>();
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             queue.add(i);
         }
 
