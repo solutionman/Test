4bd4435cf7b86fc6ad1a65b1c705b0a8d4806341
solutionman <solutionman@inbox.ru>
2023-01-19 09:57:41 +0700

    new String

--- src/stringtest/StringTest.java
+++ src/stringtest/StringTest.java
@@ -2,12 +2,18 @@ package stringtest;
 
 public class StringTest {
     public static void main(String[] args){
-        String string = "hello";
         String hi = "hello";
+        String hi1 = "hello";
+        String hiNew = new String("hello"); // this will create new object
+
+        System.out.println(hi == hi1); // hi and hi1 refers to one object in stringPool
+        System.out.println(hi1 == hiNew); // hi and hi2 different objects
+        System.out.println(hi1.equals(hiNew));
+
         String hey = "hello";
 
         // intern
-        String another =  string.intern();
+        String another =  hi.intern();
         System.out.println(another);
 
         System.out.println(concatWithoutFirst("dhe", "fllo"));
